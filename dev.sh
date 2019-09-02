8d15533d6bd40427ea5a3d5f3685ced27c1536f4
Masato Yoshioka <yoshioka0627@gmail.com>
2019-09-02 22:10:43 +0900

    add metals

--- dev.sh
+++ dev.sh
@@ -8,3 +8,25 @@ nerd_fonts() {
   cd ..
   rm -rf nerd-fonts
 }
+
+metals() {
+  curl -L -o coursier https://git.io/coursier
+  chmod +x coursier
+  ./coursier bootstrap \
+    --java-opt -Xss4m \
+    --java-opt -Xms100m \
+    --java-opt -Dmetals.client=coc.nvim \
+    org.scalameta:metals_2.12:0.7.0 \
+    -r bintray:scalacenter/releases \
+    -r sonatype:snapshots \
+    -o /usr/local/bin/metals-vim -f
+  makedir $HOME/bin
+  mv coursier $HOME/bin
+}
+
+makedir() {
+  dir=$1
+  if [ ! -d $dir ]; then
+      mkdir -p $dir
+  fi
+}
